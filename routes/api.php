00102b7e44011e7f5a26be40443cbc951f9c9284
Jack Sari <janasarii@gmail.com>
2023-01-16 01:44:53 -0500

    add

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\homeController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/prueba', [homeController::class, 'index']);
+
+// Route::get('lote/cintillos-libres',  [ApiLoteController::class, 'getFreeCintillos']);
